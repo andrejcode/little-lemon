8cc4c491f769cc6a4cb3e81501c320945a8f9ba0
Andrej <amilanovi0@gmail.com>
2023-01-02 16:56:53 +0100

    Add routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Homepage';
+import Booking from './BookingPage';
+import About from './About';
+import ConfirmPage from './ConfirmPage';
+
 function Main() {
   return (
-    <main></main>
+    <main>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/about" element={<About />} />
+        <Route path="confirm" element={<ConfirmPage />} />
+      </Routes>
+    </main>
   );
 }
 
